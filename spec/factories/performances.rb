ed6705dc7be6f5753762cd24b405dfb26ea8dd6a
tnandate <nandate.takuya@moneyforward.co.jp>
2018-10-14 12:57:32 +0900

    setup first spec

---
+++ spec/factories/performances.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :performance do
+    event nil
+    start_time "2018-10-10 20:00:00"
+    end_time "2018-10-10 20:30:00"
+  end
+end
---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,9 @@
+require 'factory_bot'
+
+# Setup factory bot
+FactoryBot.definition_file_paths << Rails.root.join('spec', 'factories')
+FactoryBot.reload
+
+RSpec.configure do |config|
+  config.include(FactoryBot::Syntax::Methods)
+end

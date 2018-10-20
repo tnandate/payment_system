c2d2bd7fd7be3465faefdeebcc85d2232e5df29f
tnandate <nandate.takuya@moneyforward.co.jp>
2018-10-20 22:49:47 +0900

    shopping_cart#update

---
+++ spec/controllers/shopping_cart_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe ShoppingCartsController do
+  describe '#update' do
+    let(:user) { instance_spy(:user) }
+    let(:performance) { instance_spy(
+      Performance, event: build_stubbed(:event)) }
+    let(:action) { instance_spy(AddsToCart) }
+
+    before(:example) do
+      allow(@controller).to receive(:current_user).and_return(user)
+      allow(Performance).to receive(:find).with('2').and_return(performance)
+      allow(AddsToCart).to receive(:new).with(
+        performance: performance, count: '1', user: user).and_return(action)
+    end
+
+    it 'calls add to cart no a successful' do
+      allow(action).to receive(:success).and_return(true)
+      patch :update, params: { performance_id: '2', ticket_count: '1' }
+      expect(action).to have_received(:run)
+      expect(@controller).to redirect_to(shopping_cart_path)
+    end
+
+    it 'redirects back to the event if unsuccessful' do
+      allow(action).to receive(:success).and_return(false)
+      patch :update, params: { performance_id: '2', ticket_count: '2' }
+      expect(action).to have_received(:run)
+      expect(@controller).to redirect_to(performance.event)
+    end
+  end
+end

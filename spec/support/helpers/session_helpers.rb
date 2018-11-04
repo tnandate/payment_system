7f4166d1a9d2f7d9d9655055a660adb5281168f1
tnandate <nandate.takuya@moneyforward.co.jp>
2018-11-04 19:48:00 +0900

    add feature test of user sign in

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -8,5 +8,12 @@ module Features
       fill_in "user_password_confirmation", with: confirmation
       click_button "Sign up"
     end
+
+    def signin(email, password)
+      visit new_user_session_path
+      fill_in "user_email", with: email
+      fill_in "user_password", with: password
+      click_button "Log in"
+    end
   end
 end

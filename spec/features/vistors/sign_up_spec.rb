8202de01c770f1dc0d6522c03beaea76707d6bd4
tnandate <nandate.takuya@moneyforward.co.jp>
2018-11-04 17:13:36 +0900

    add feature test of user sign up

--- spec/features/vistors/sign_up_spec.rb
+++ spec/features/vistors/sign_up_spec.rb
@@ -1,8 +1,8 @@
-feature 'Sign Up', :devise do
+feature 'Sign Up' do
   scenario 'visitor can sign up with valid email address and password' do
-    sign_up_with('test user','test@example.com', 'password', 'password')
-    txts = [I18n.t('devise.registrations.signed_up'),
-            I18n.t('devise.registrations.signed_up_but_unconfirmed')]
+    sign_up_with('test user', 'test@example.com', 'please123', 'please123')
+    txts = [I18n.t("devise.registrations.signed_up"),
+            I18n.t("devise.registrations.signed_up_but_unconfirmed")]
     expect(page).to have_content(/.*#{txts[0]}.*|.*#{txts[1]}.*/)
-    end
+  end
 end
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,10 +2,10 @@ module Features
   module SessionHelpers
     def sign_up_with(name, email, password, confirmation)
       visit new_user_registration_path
-      fill_in "Name", with: name
-      fill_in "Email", with: email
-      fill_in "Password", with: password
-      fill_in "Password confirmation", with: confirmation
+      fill_in "user_name", with: name
+      fill_in "user_email", with: email
+      fill_in "user_password", with: password
+      fill_in "user_password_confirmation", with: confirmation
       click_button "Sign up"
     end
   end

9f203eaa4a3387cae12eedf8546973f7ab2f5710
tnandate <nandate.takuya@moneyforward.co.jp>
2018-11-25 23:47:59 +0900

    fix stripe_token.rb

--- app/models/stripe_token.rb
+++ app/models/stripe_token.rb
@@ -1,16 +1,31 @@
 class StripeToken
-  attr_accessor :credit_card_number, :expiration_month, :expiration_year, :cvc
+  attr_accessor :credit_card_number, :expiration_month, :expiration_year, :cvc, :stripe_token
   delegate :id, to: :token
 
-  def initialize(credit_card_number:, expiration_month:, expiration_year:, cvc:)
+  def initialize(credit_card_number: nil, expiration_month: nil, expiration_year: nil, cvc: nil, stripe_token: nil)
     @credit_card_number = credit_card_number
     @expiration_month   = expiration_month
     @expiration_year    = expiration_year
     @cvc                = cvc
+    @stripe_token       = stripe_token
   end
 
   def token
-    @token ||= Stripe::Token.create(
+    @token ||= stripe_token ? retrive_token : create_token
+  end
+
+  def id
+    stripe_token || token.id
+  end
+
+  private
+
+  def retrive_token
+    Stripe::Token.retrive(stripe_token)
+  end
+
+  def create_token
+    Stripe::Token.create(
       card: {
         number: credit_card_number,
         exp_month: expiration_month,
